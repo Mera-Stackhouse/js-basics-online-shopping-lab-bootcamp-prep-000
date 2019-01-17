dd604481f050856d836f21c4d013d8a606d6d789
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 23:52:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -35,6 +35,7 @@ function viewCart() {
 
 function total() {
   // write your code here
+  console.log(cart.length)
   for(i=0; i < cart.length; i++) {
     var totalCost = 0 
     totalCost = totalCost + cart[i].itemPrice
