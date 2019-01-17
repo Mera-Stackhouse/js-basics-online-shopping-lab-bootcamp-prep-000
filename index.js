bc73ecc6df60dea356735839b4042f7fc39a217a
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-17 23:06:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ function addToCart(item) {
  // write your code here
  var number = Math.floor(Math.random() * Math.floor(101));
  cart[`itemName: ${item}`] = `itemPrice: ${number}`;
+ return `${item} has been added to your cart.`;
 }
 
 function viewCart() {
   // write your code here
+  if (cart.lenth = 0) {
+    return 'Your shopping cart is empty.';
+  } else {
+    var string = 'In your cart, you have ';
+    for (i=0; i < ; i++) {
+      string = string + cart[]
+    }
+  }
 }
 
 function total() {
