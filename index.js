34579af0e8cd1447dd7b33735bd47a59be1ebba1
Mera Stackhouse <mera.stackhouse@gmail.com>
2019-01-18 00:01:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -48,10 +48,9 @@ function removeFromCart(item) {
     if (item === cart[i].itemName) {
       cart.splice(i, 1)
       return cart
-    } else {
-      return 'That item is not in your cart.'
-    
+    }
   }
+  return 'That item is not in your cart.'
 }
 
 function placeOrder(cardNumber) {
